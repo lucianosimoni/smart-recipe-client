82fa3e27322711d2183cad7c8e69467d69142d37
Luciano Simoni <lucianosimonipersonal@gmail.com>
2023-06-17 20:48:39 +0100

    About me Layout Fix + Logo Link - About me now works fine and as it should in Mobile devices (smaller screens). - The logo "Smart Recipe" now has a Link that takes the user to the Home page when clicked.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,20 +6,22 @@ export default function About() {
     <main className="flex min-h-screen flex-col items-center justify-between p-8 sm:p-24 overflow-hidden">
       {/* ✉️ Title */}
       <div className="flex place-items-center after:duration-[12000ms] after:animate-[pulse_5s_ease-in-out_infinite] before:pointer-events-none after:pointer-events-none before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:bg-green-300 after:via-green-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-green-700 before:dark:opacity-10 after:dark:from-green-900 after:dark:via-green-400 after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert mb-8"
-          src="/smart-recipe.svg"
-          alt="Smart Recipe Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <Link href="/">
+          <Image
+            className="dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert mb-8"
+            src="/smart-recipe.svg"
+            alt="Smart Recipe Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </Link>
       </div>
 
       {/* 📦 Middle Grid */}
       <div className="w-full max-w-5xl flex flex-col text-center gap-16 lg:flex-row lg:text-left lg:place-items-center">
         {/* 🥬 Text */}
-        <div className="flex flex-col items-center w-2/3">
+        <div className="flex flex-col items-center w-full lg:w-2/3">
           <h1 className="m-0 max-w-[30ch] text-2xl font-bold mb-4 lg: text-center">
             Welcome to our Recipe App! 😄
           </h1>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,14 +6,16 @@ export default function Login() {
     <main className="flex min-h-screen flex-col items-center justify-between p-8 sm:p-24 overflow-hidden">
       {/* ✉️ Title */}
       <div className="flex place-items-center after:duration-[12000ms] after:animate-[pulse_5s_ease-in-out_infinite] before:pointer-events-none after:pointer-events-none before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:bg-green-300 after:via-green-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-green-700 before:dark:opacity-10 after:dark:from-green-900 after:dark:via-green-400 after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert mb-8"
-          src="/smart-recipe.svg"
-          alt="Smart Recipe Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <Link href="/">
+          <Image
+            className="dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert mb-8"
+            src="/smart-recipe.svg"
+            alt="Smart Recipe Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </Link>
       </div>
 
       {/* ✅ Form & Buttons */}
